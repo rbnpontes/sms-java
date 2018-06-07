1089d63c7d7bae46249bccef3fee9a3795c3b74f
felipemoura2017 <felipe.h.moura2017@gmail.com>
2018-06-07 16:24:04 -0300

    Class Contact Manager Comentada

--- com.sms.business/src/com/sms/business/ContactManager.java
+++ com.sms.business/src/com/sms/business/ContactManager.java
@@ -5,37 +5,41 @@ import java.util.List;
 
 import com.sms.entities.*;
 import com.sms.repository.IDBModel;
-
+///Contact Manager Gerencia os Contatos
 public class ContactManager {
 	private static ContactManager instance;
-
+	
 	public ContactManager() {
 		instance = this;
 	}
-
+	
 	public static ContactManager getSingleton() {
 		return instance;
 	}
-
+	/// Adiciona os contatos
 	public void addNewContact(Contact contact) throws SQLException {
+		///Roda a query de inserir contatos
 		GlobalManager.getDatabase().exec(
 				"INSERT INTO contact (id_target,id_owner)\r\n" + 
 				"VALUES\r\n" + 
 				"("+contact.id_target+","+contact.id_owner+"),\r\n" + 
 				"("+contact.id_owner+","+contact.id_target+")");
 	}
-
+	/// Remove os contatos pelo id
 	public void deleteContactById(int id) throws SQLException {
+		///Roda a query de deletar contatos pelo id
 		GlobalManager.getDatabase().exec("DELETE FROM contact WHERE id=" + id);
 	}
+	///Remove os contatos pelo objeto
 	public void deleteContact(Contact cnt) throws SQLException{
+		///Roda a query de deletar contatos pelo objeto
 		String query = "DELETE FROM contact\r\n" + 
 				"WHERE\r\n" + 
 				"(id_target = "+cnt.id_target+" AND id_owner = "+cnt.id_owner+")\r\n" + 
 				"OR\r\n" + 
 				"(id_target = "+cnt.id_owner+" AND id_owner= "+cnt.id_target+")";
 		GlobalManager.getDatabase().exec(query);
-	}
+	}/// Busca contatos pelo id de um Usuário
 	public Contact[] getContacts(int userId) throws SQLException {
 		/// Executa a Query onde ira trazer todos os Usuarios registrados no Banco
 		List<IDBModel> results = GlobalManager.getDatabase().executeQuery("SELECT * FROM contact WHERE id_owner="+ userId,Contact.class);
@@ -45,15 +49,16 @@ public class ContactManager {
 		for (int i = 0; i < results.size(); i++)
 			contact[i] = (Contact) results.get(i); /// Converte de DBModel para User
 		return contact;
-		}
+		}/// Buscar contatos pelo proprio id
 	public Contact getContactById(int id) throws SQLException {
 		List<IDBModel> results = GlobalManager.getDatabase().executeQuery("SELECT * FROM contacts WHERE id="+id, Contact.class);
 		/// Verifica-se a quantidade de resultados retornado do banco é maior que zero
 		if(results.size() == 0)
 			return null;
 		return (Contact)results.get(0);
-	}
+	}/// Busca contatos de um Usuário
 	public Contact[] getUserContacts(User user) {
+		// Roda a Query para buscar contatos de um usuário
 		String query = "SELECT\r\n" + 
 				"contact.id AS id,\r\n" + 
 				"owner.id AS owner_id,\r\n" + 
@@ -68,6 +73,7 @@ public class ContactManager {
 				"WHERE\r\n" + 
 				"owner.id = "+user.id;
 		try {
+			//Consulta e preenche os valores no objeto
 			List<IDBModel> results = GlobalManager.getDatabase().executeQuery(query, ContactInfo.class);
 			Contact[] contacts = new Contact[results.size()];
 			/// Fill Models
